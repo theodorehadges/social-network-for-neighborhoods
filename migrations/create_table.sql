07bef64c6c71a43f7e00ab3e2041ec8247b1c80d
mrpoor <greg15001@gmail.com>
2019-12-03 20:55:02 -0500

    Added some not null constraints

--- migrations/create_table.sql
+++ migrations/create_table.sql
@@ -1,27 +1,32 @@
 create table neighborhood(id serial primary key, name varchar);
-create table block(id serial primary key , name varchar, neighborhood_id int references neighborhood(id));
-create table userm(id serial primary key , firstname varchar, lastname varchar, email varchar, street varchar,
+create table block(id serial primary key , name varchar, neighborhood_id int references neighborhood(id) not null);
+create table userm(id serial primary key , firstname varchar, lastname varchar, email varchar not null, street varchar,
     city varchar, state char(2), zipcode int, lat float, long float, block_id int references block(id), created_on timestamp);
-create table profile(id serial primary key, description varchar, photo varchar, user_id int references userm(id) unique, created_on timestamp);
-create table friend(id serial primary key , user_1_id int references userm(id), user_2_id int references userm(id),
-    unique (user_1_id, user_2_id), created_on timestamp);
-create table friend_request(id serial primary key , user_1_id int references userm(id), user_2_id int references userm(id),
-approved bool, created_on timestamp);
---this one is weird
-create table block_apply(id serial primary key , pending_user int references userm(id), need_approval_by int references userm(id),
-    given_approval bool, created_on timestamp, decided_on timestamp);
-create table block_user(id serial primary key, block_id int references block(id), user_id int references userm(id));
-create table neighbor(id serial primary key , user_1_id int references userm(id), user_2_id int references userm(id));
-create table thread(id serial primary key , name varchar, created_on timestamp);
-create table thread_message(id serial primary key , thread_id int references thread(id), author int references userm(id),
-    created_time timestamp, title varchar, body varchar, lat float, long float, in_reply_to_message_id int references thread_message(id));
-create table thread_friend(id serial primary key, thread_id int references thread(id),friend_id int references friend(id));
-create table thread_neighbor(id serial primary key, thread_id int references thread(id), neighbor_id int references neighbor(id));
-create table thread_block(id serial primary key, thread_id int references thread(id), block_id int references block(id));
-create table thread_neighborhood(id serial primary key, thread_id int references thread(id), neighborhood_id int references neighborhood(id));
-create table message_read(id serial primary key, message_id int references thread_message(id),
-user_id int references userm(id), read bool);
-create table user_log(id serial primary key, user_id int references userm(id), login_time timestamp);
+create table profile(id serial primary key, description varchar, photo varchar,
+user_id int references userm(id) unique not null, created_on timestamp);
+create table friend(id serial primary key , user_1_id int references userm(id) not null,
+user_2_id int references userm(id) not null, unique (user_1_id, user_2_id), created_on timestamp);
+create table friend_request(id serial primary key , user_1_id int references userm(id) not null,
+user_2_id int references userm(id) not null, approved bool, created_on timestamp);
+create table block_apply(id serial primary key , pending_user int references userm(id) not null,
+need_approval_by int references userm(id) not null, given_approval bool, block_id int references block(id) not null,
+created_on timestamp, decided_on timestamp);
+create table neighbor(id serial primary key, user_1_id int references userm(id) not null,
+user_2_id int references userm(id) not null);
+create table thread(id serial primary key, name varchar, created_on timestamp);
+create table thread_message(id serial primary key , thread_id int references thread(id) not null,
+author int references userm(id), created_time timestamp, title varchar, body varchar, lat float, long float, in_reply_to_message_id int references thread_message(id));
+create table thread_friend(id serial primary key, thread_id int references thread(id) not null,
+friend_id int references friend(id) not null);
+create table thread_neighbor(id serial primary key, thread_id int references thread(id) not null,
+neighbor_id int references neighbor(id) not null);
+create table thread_block(id serial primary key, thread_id int references thread(id) not null,
+block_id int references block(id) not null);
+create table thread_neighborhood(id serial primary key, thread_id int references thread(id) not null,
+neighborhood_id int references neighborhood(id) not null);
+create table message_read(id serial primary key, message_id int references thread_message(id) not null,
+user_id int references userm(id) not null, read bool);
+create table user_log(id serial primary key, user_id int references userm(id) not null, login_time timestamp);
 
 
 drop table if exists user_log;
